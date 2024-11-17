575eec2a2620c872c2da891175e26d49a0aec125
Ulises Sanchez <wulisesbusiness@gmail.com>
2024-11-16 21:37:20 -0600

    Modificando las variables de entorno para postgers en la nubex3

--- src/app/page.js
+++ src/app/page.js
@@ -1,24 +1,26 @@
 async function getData() {
-  const res = await fetch('http://localhost:3000/api/test');
-  if (!res.ok) {
-      throw new Error('Error al obtener los datos');
+    const apiUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000/api/test'; // Usa la URL de producción si está disponible
+    const res = await fetch(apiUrl);
+    if (!res.ok) {
+        throw new Error('Error al obtener los datos');
+    }
+    return res.json();
   }
-  return res.json();
-}
-
-export default async function Home() {
-  const data = await getData();
-
-  return (
-      <main style={{ padding: '20px', textAlign: 'center' }}>
-          <h1>Waifus desde PostgreSQL</h1>
-          <ul>
-              {data.map((waifu, index) => (
-                  <li key={index}>
-                      {waifu.nombre} - {waifu.codename}
-                  </li>
-              ))}
-          </ul>
-      </main>
-  );
-}
+  
+  export default async function Home() {
+    const data = await getData();
+  
+    return (
+        <main style={{ padding: '20px', textAlign: 'center' }}>
+            <h1>Waifus desde PostgreSQL</h1>
+            <ul>
+                {data.map((waifu, index) => (
+                    <li key={index}>
+                        {waifu.nombre} - {waifu.codename}
+                    </li>
+                ))}
+            </ul>
+        </main>
+    );
+  }
+  
