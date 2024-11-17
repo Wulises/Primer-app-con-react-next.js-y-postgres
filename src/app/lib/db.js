cfe616f20e13fe0de7cf6b3dd61eb8b38cb2f064
Ulises Sanchez <wulisesbusiness@gmail.com>
2024-11-16 21:16:42 -0600

    Modificando las variables de entorno para postgers en la nube

--- src/app/lib/db.js
+++ src/app/lib/db.js
@@ -1,5 +1,20 @@
 import { Pool } from 'pg';
 
+const pool = new Pool({
+  user: process.env.DB_USER,         // Usuario de la base de datos
+  host: process.env.DB_HOST,         // Dirección del host de Render
+  database: process.env.DB_DATABASE, // Nombre de la base de datos
+  password: process.env.DB_PASSWORD, // Contraseña
+  port: process.env.DB_PORT || 5432, // Puerto
+});
+
+export default pool;
+
+
+
+
+/*import { Pool } from 'pg';
+
 // Configuración de conexión a PostgreSQL
 const pool = new Pool({
     user: 'postgres',         // Usuario de PostgreSQL
