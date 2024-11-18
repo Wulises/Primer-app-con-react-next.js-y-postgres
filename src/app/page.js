2318007177f47038cf2ce9360cc3660719d81023
Ulises Sanchez <wulisesbusiness@gmail.com>
2024-11-17 22:15:54 -0600

    Ahora si conexion a la base de datos de render x6

--- src/app/page.js
+++ src/app/page.js
@@ -1,28 +1,30 @@
-async function getData() {
-    const apiUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000/api/test'; // URL de tu API local
-    const res = await fetch(apiUrl);
-    if (!res.ok) {
-      throw new Error('Error al obtener los datos');
-    }
-    return res.json();
-  }
-  
-  export default async function Home() {
-    const data = await getData();
-  
-    return (
-      <main style={{ padding: '20px', textAlign: 'center' }}>
-        <h1>Waifus desde PostgreSQL</h1>
-        <ul>
-          {data.map((waifu, index) => (
-            <li key={index}>
-              {waifu.nombre} - {waifu.codename} - {waifu.persona}
-            </li>
-          ))}
-        </ul>
-      </main>
-    );
-  }
-  
+// src/app/page.js
+import React from 'react';
 
+async function fetchData() {
+  const apiUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000/api/test';
+  const res = await fetch(apiUrl);
   
+  if (!res.ok) {
+    throw new Error('Error al obtener los datos');
+  }
+
+  return res.json();
+}
+
+export default async function Home() {
+  const data = await fetchData();
+
+  return (
+    <main style={{ padding: '20px', textAlign: 'center' }}>
+      <h1>Waifus desde PostgreSQL</h1>
+      <ul>
+        {data.map((waifu, index) => (
+          <li key={index}>
+            {waifu.nombre} - {waifu.codename} - {waifu.persona}
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
+}
