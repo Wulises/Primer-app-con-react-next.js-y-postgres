c0ffde9be5eb80eab2a132387d3a1f3e0aee347b
Ulises Sanchez <wulisesbusiness@gmail.com>
2024-11-17 22:32:09 -0600

    Ahora si conexion a la base de datos de render x7

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+  const nextConfig = {
+    experimental: {
+      appDir: true, // Habilita la carpeta 'app/' experimental
+    },
+  };
+  
+  export default nextConfig;
+  
